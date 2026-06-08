d2a538d17b4a4b628f20d7b321d8a7758cbe2fdc
PhamHuuDoi <doipham1812@gmai.com>
2026-06-08 18:19:12 +0700

    update databse add table refesh token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,49 +1,49 @@
-const {
-  verifyToken,
-} = require("../config/jwt");
+  const {
+    verifyToken,
+  } = require("../config/jwt");
 
-function authMiddleware(
-  req,
-  res,
-  next,
-) {
-
-  const auth =
-    req.headers.authorization;
-
-  if (
-    !auth ||
-    !auth.startsWith("Bearer ")
+  function authMiddleware(
+    req,
+    res,
+    next,
   ) {
-    return res.status(401).json({
-      error: "Unauthorized",
-    });
+
+    const auth =
+      req.headers.authorization;
+
+    if (
+      !auth ||
+      !auth.startsWith("Bearer ")
+    ) {
+      return res.status(401).json({
+        error: "Unauthorized",
+      });
+    }
+
+    try {
+
+      const token =
+        auth.split(" ")[1];
+
+      const payload =
+        verifyToken(token);
+
+      req.user = {
+        id: payload.sub,
+        role: payload.role,
+      };
+
+      next();
+
+    } catch {
+
+      return res.status(401).json({
+        error: "Invalid token",
+      });
+
+    }
   }
 
-  try {
-
-    const token =
-      auth.split(" ")[1];
-
-    const payload =
-      verifyToken(token);
-
-    req.user = {
-      id: payload.sub,
-      role: payload.role,
-    };
-
-    next();
-
-  } catch {
-
-    return res.status(401).json({
-      error: "Invalid token",
-    });
-
-  }
-}
-
-module.exports = {
-  authMiddleware,
-};
+  module.exports = {
+    authMiddleware,
+  };
